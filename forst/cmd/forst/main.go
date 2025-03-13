a841565bde3316ec6221db5ff41bf6a179b65876
haveyaseen <haveyaread@gmail.com>
2025-03-13 13:43:00 +0100

    Refactor main.go

--- forst/cmd/forst/main.go
+++ forst/cmd/forst/main.go
@@ -10,74 +10,95 @@ import (
 	"forst/pkg/transformers"
 	"os"
 )
-func main() {
-	// Parse command line flags
+
+func parseFlags() (string, bool) {
 	debug := flag.Bool("debug", false, "Enable debug output")
 	flag.Parse()
 
 	args := flag.Args()
 	if len(args) < 1 {
 		fmt.Println("Usage: forst <filename>.ft")
+		return "", false
+	}
+	return args[0], *debug
+}
+
+func readSourceFile(filePath string) ([]byte, error) {
+	source, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+	return source, nil
+}
+
+func debugPrintTokens(tokens []ast.Token) {
+	fmt.Println("\n=== Tokens ===")
+	for _, t := range tokens {
+		fmt.Printf("%s:%d:%d: %-12s '%s'\n",
+			t.Path, t.Line, t.Column,
+			t.Type, t.Value)
+	}
+}
+
+func debugPrintForstAST(forstAST ast.FuncNode) {
+	fmt.Println("\n=== Forst AST ===")
+	fmt.Printf("Function: %s -> %s\n", forstAST.Name, forstAST.ReturnType)
+	fmt.Println("Body:")
+	for _, node := range forstAST.Body {
+		switch n := node.(type) {
+		case ast.AssertNode:
+			fmt.Printf("  Assert: %s or %s\n", n.Condition, n.ErrorType)
+		case ast.ReturnNode:
+			fmt.Printf("  Return: %s\n", n.Value)
+		}
+	}
+}
+
+func debugPrintGoAST(goAST ast.FuncNode) {
+	fmt.Println("\n=== Go AST ===")
+	fmt.Printf("Function: %s -> %s\n", goAST.Name, goAST.ReturnType)
+	fmt.Println("Body:")
+	for _, node := range goAST.Body {
+		switch n := node.(type) {
+		case ast.AssertNode:
+			fmt.Printf("  Assert: %s\n", n.Condition)
+		case ast.ReturnNode:
+			fmt.Printf("  Return: %s\n", n.Value)
+		}
+	}
+}
+
+func main() {
+	filePath, debug := parseFlags()
+	if filePath == "" {
 		return
 	}
 
-	// Read the Forst file
-	filePath := args[0]
-	source, err := os.ReadFile(filePath)
+	source, err := readSourceFile(filePath)
 	if err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
+		fmt.Println(err)
 		return
 	}
 
 	// Compilation pipeline
 	tokens := lexer.Lexer(source, lexer.Context{FilePath: filePath})
-	
-	if *debug {
-		fmt.Println("\n=== Tokens ===")
-		for _, t := range tokens {
-			fmt.Printf("%s:%d:%d: %-12s '%s'\n",
-				t.Path, t.Line, t.Column,
-				t.Type, t.Value)
-		}
+	if debug {
+		debugPrintTokens(tokens)
 	}
 
-	parser := parser.NewParser(tokens)
-	forstAST := parser.Parse()
-
-	if *debug {
-		fmt.Println("\n=== Forst AST ===")
-		fmt.Printf("Function: %s -> %s\n", forstAST.Name, forstAST.ReturnType)
-		fmt.Println("Body:")
-		for _, node := range forstAST.Body {
-			switch n := node.(type) {
-			case ast.AssertNode:
-				fmt.Printf("  Assert: %s or %s\n", n.Condition, n.ErrorType)
-			case ast.ReturnNode:
-				fmt.Printf("  Return: %s\n", n.Value)
-			}
-		}
+	forstAST := parser.NewParser(tokens).Parse()
+	if debug {
+		debugPrintForstAST(forstAST)
 	}
 
 	goAST := transformers.TransformForstToGo(forstAST)
-
-	if *debug {
-		fmt.Println("\n=== Go AST ===")
-		fmt.Printf("Function: %s -> %s\n", goAST.Name, goAST.ReturnType)
-		fmt.Println("Body:")
-		for _, node := range goAST.Body {
-			switch n := node.(type) {
-			case ast.AssertNode:
-				fmt.Printf("  Assert: %s\n", n.Condition)
-			case ast.ReturnNode:
-				fmt.Printf("  Return: %s\n", n.Value)
-			}
-		}
+	if debug {
+		debugPrintGoAST(goAST)
 	}
 
 	goCode := generators.GenerateGoCode(goAST)
 
-	// Output generated Go code
-	if *debug {
+	if debug {
 		fmt.Println("\n=== Generated Go Code ===")
 	}
 	fmt.Println(goCode)
