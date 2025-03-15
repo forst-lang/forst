9ed16822f5995d793bdeb0909a17b9619f600b16
haveyaseen <haveyaread@gmail.com>
2025-03-15 19:02:43 +0100

    Extract parsing of ensure statement

---
+++ forst/pkg/parser/ensure.go
@@ -0,0 +1,63 @@
+package parser
+
+import (
+	"forst/pkg/ast"
+)
+
+func (p *Parser) parseEnsureStatement(context *Context) ast.EnsureNode {
+	p.advance() // Move past `ensure`
+
+	var variable string
+	var assertion ast.AssertionNode
+
+	// Handle special case for negated variable check
+	if p.current().Type == ast.TokenLogicalNot && p.peek().Type == ast.TokenIdentifier {
+		p.advance() // Move past !
+		if p.peek().Type == ast.TokenLParen {
+			panic(parseErrorWithValue(p.current(), "Expected variable after ensure !"))
+		}
+		variable = p.current().Value
+		p.advance() // Move past variable
+		// Create implicit Nil() assertion
+		errorType := ast.TypeError
+		assertion = ast.AssertionNode{
+			BaseType: &errorType,
+			Constraints: []ast.ConstraintNode{
+				{
+					Name: "Nil",
+					Args: []ast.ValueNode{},
+				},
+			},
+		}
+	} else {
+		variable = p.expect(ast.TokenIdentifier).Value
+
+		p.expect(ast.TokenIs)
+
+		assertion = p.parseAssertionChain(context)
+	}
+
+	if !context.IsMainFunction() || p.current().Type == ast.TokenOr {
+		p.expect(ast.TokenOr) // Expect `or`
+
+		errorType := p.expect(ast.TokenIdentifier).Value
+		var err ast.EnsureErrorNode
+		if p.current().Type == ast.TokenLParen {
+			p.advance() // Consume left paren
+			var args []ast.ExpressionNode
+			for p.current().Type != ast.TokenRParen {
+				args = append(args, p.parseExpression(context))
+				if p.current().Type == ast.TokenComma {
+					p.advance()
+				}
+			}
+			p.expect(ast.TokenRParen)
+			err = ast.EnsureErrorCall{ErrorType: errorType, ErrorArgs: args}
+		} else {
+			err = ast.EnsureErrorVar(errorType)
+		}
+		return ast.EnsureNode{Variable: variable, Assertion: assertion, Error: &err}
+	}
+
+	return ast.EnsureNode{Variable: variable, Assertion: assertion}
+}
